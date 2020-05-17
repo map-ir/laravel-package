ebb682422a878687ad7b4425367c050ae4945481
Alireza <aa.maesum@gmail.com>
2020-05-18 02:18:29 +0430

    added search v2

--- src/MapirLaravel.php
+++ src/MapirLaravel.php
@@ -98,5 +98,79 @@ class MapirLaravel
             return json_decode($e->getResponse()->getBody());
         }
     }
+    public static function search_v2($text, $select = "" , $filter = "" , $lat = 0 , $lon = 0)
+    {
+        try{
+            $client= new Client();
+            $body=[
+                'text' =>$text,
+                '$select' => $select,
+                '$filter' => $filter,
+                'lat'=> $lat,
+                'lon'=>$lon
+            ];
+
+            if ($select == ""){
+                unset($body['$select']);
+            }
+            if ($filter == ""){
+                unset($body['$filter']);
+            }
+            if ($lat == 0 or $lon == 0){
+                unset($body['lon']);
+                unset($body['lat']);
+            }
+
+            $header=[
+                'x-api-key' => config('mapir.x-api-key','Your map.ir api key'),
+                'Content-Type' => 'application/json'
+            ];
+            $result= $client->request(
+                'POST',
+                config('mapir.webservice-url','https://map.ir').'/search/v2',
+                ['headers'=>$header, 'json'=>$body]
+            );
+            return json_decode($result->getBody(),true);
+        } catch (RequestException $e) {
+            return json_decode($e->getResponse()->getBody());
+        }
+    }
+    public static function searchAutocomplete_v2($text, $select = "" , $filter = "" , $lat = 0 , $lon = 0)
+    {
+        try{
+            $client= new Client();
+            $body=[
+                'text' =>$text,
+                '$select' => $select,
+                '$filter' => $filter,
+                'lat'=> $lat,
+                'lon'=>$lon
+            ];
+
+            if ($select == ""){
+                unset($body['$select']);
+            }
+            if ($filter == ""){
+                unset($body['$filter']);
+            }
+            if ($lat == 0 or $lon == 0){
+                unset($body['lon']);
+                unset($body['lat']);
+            }
+
+            $header=[
+                'x-api-key' => config('mapir.x-api-key','Your map.ir api key'),
+                'Content-Type' => 'application/json'
+            ];
+            $result= $client->request(
+                'POST',
+                config('mapir.webservice-url','https://map.ir').'/search/v2/autocomplete',
+                ['headers'=>$header, 'json'=>$body]
+            );
+            return json_decode($result->getBody(),true);
+        } catch (RequestException $e) {
+            return json_decode($e->getResponse()->getBody());
+        }
+    }
 
 }
